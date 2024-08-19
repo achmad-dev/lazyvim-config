38cd4b5a21f09946c5c55a7bfb289533d94c76e5
achmad <faa04807@gmail.com>
2024-08-20 00:04:56 +0700

    chore: add ocaml

---
+++ lua/plugins/ocaml.lua
@@ -0,0 +1,33 @@
+return {
+    {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+        if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "ocaml" })
+        end
+    end,
+    },
+    {
+    "neovim/nvim-lspconfig",
+    opts = {
+        servers = {
+        ocamllsp = {
+            get_language_id = function(_, ftype)
+            return language_id_of[ftype]
+            end,
+            root_dir = function(fname)
+            return require("lspconfig.util").root_pattern(
+                "*.opam",
+                "esy.json",
+                "package.json",
+                ".git",
+                "dune-project",
+                "dune-workspace",
+                "*.ml"
+            )(fname)
+            end,
+        },
+        },
+    },
+    }
+}
