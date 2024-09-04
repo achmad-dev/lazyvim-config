ab90df54b331822f77b2f680f38d2bd5433363bb
achmad <faa04807@gmail.com>
2024-09-04 12:59:30 +0700

    chore(migrate plugin): migrate formatter to mhartington/formatter

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,81 +1,100 @@
 return {
     {
-        "stevearc/conform.nvim",
-        dependencies = { "mason.nvim" },
-        lazy = true,
-        cmd = "ConformInfo",
-        keys = {
-            {
-            "<leader>cF",
-            function()
-                require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
-            end,
-            mode = { "n", "v" },
-            desc = "Format Injected Langs",
-            },
-        },
-        init = function()
-            -- Install the conform formatter on VeryLazy
-            LazyVim.on_very_lazy(function()
-            LazyVim.format.register({
-                name = "conform.nvim",
-                priority = 100,
-                primary = true,
-                format = function(buf)
-                require("conform").format({ bufnr = buf })
-                end,
-                sources = function(buf)
-                local ret = require("conform").list_formatters(buf)
-                ---@param v conform.FormatterInfo
-                return vim.tbl_map(function(v)
-                    return v.name
-                end, ret)
-                end,
+        "williamboman/mason.nvim",
+        opts = { ensure_installed = { "clang-format", "gofumpt", "ruff", "ocamlformat", "black", "isort" } },
+    },
+    {
+        "mhartington/formatter.nvim",
+        config = function()
+            -- Utilities for creating configurations
+            local util = require "formatter.util"
+
+            -- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
+            require("formatter").setup({
+                -- Enable or disable logging
+                logging = true,
+                -- Set the log level
+                log_level = vim.log.levels.DEBUG,
+                -- All formatter configurations are opt-in
+                filetype = {
+                    lua = {
+                        require("formatter.filetypes.lua").stylua,
+                        function()
+                            return {
+                                exe = "stylua",
+                                args = {
+                                    "--column-width",
+                                    "120",
+                                    "--indent-type",
+                                    "Spaces",
+                                    "--indent-width",
+                                    "2",
+                                    "--search-parent-directories",
+                                    "--stdin-filepath",
+                                    util.escape_path(util.get_current_buffer_file_path()),
+                                    "--",
+                                    "-",
+                                },
+                                stdin = true,
+                            }
+                        end
+                    },
+                    java = {
+                        function()
+                            return {
+                                -- Need to install clang-format first
+                                exe = "clang-format",  -- Use clang-format for Java formatter
+                                args = {
+                                    "--style=file",
+                                    "--assume-filename",
+                                    util.escape_path(util.get_current_buffer_file_path()),
+                                },
+                                stdin = true,
+                            }
+                        end
+                    },
+                    go = {
+                        function()
+                            return {
+                                exe = "gofumpt",  -- Use gofumpt for Go files
+                                args = { "-s" },
+                                stdin = true,
+                            }
+                        end
+                    },
+                    rust = {
+                        function()
+                            return {
+                                exe = "ruff",  -- Use rustfmt for Rust files
+                                args = { "--emit=stdout" },
+                                stdin = true,
+                            }
+                        end
+                    },
+                    python = {
+                        function()
+                            return {
+                                exe = "black",  -- Use black for Python files
+                                args = { "-" },
+                                stdin = true,
+                            }
+                        end,
+                        function()
+                            return {
+                                exe = "isort",  -- Use isort for sorting imports in Python
+                                args = { "--stdout", "--profile", "black", "-" },
+                                stdin = true,
+                            }
+                        end,
+                    },
+                    ["*"] = {
+                        require("formatter.filetypes.any").remove_trailing_whitespace,
+                        function()
+                            vim.lsp.buf.format({ async = true })
+                        end,
+                    }
+                }
             })
-            end)
-        end,
-        opts = function()
-            local plugin = require("lazy.core.config").plugins["conform.nvim"]
-            if plugin.config ~= M.setup then
-            LazyVim.error({
-                "Don't set `plugin.config` for `conform.nvim`.\n",
-                "This will break **LazyVim** formatting.\n",
-                "Please refer to the docs at https://www.lazyvim.org/plugins/formatting",
-            }, { title = "LazyVim" })
-            end
-            ---@type conform.setupOpts
-            local opts = {
-            default_format_opts = {
-                timeout_ms = 3000,
-                async = false, -- not recommended to change
-                quiet = false, -- not recommended to change
-                lsp_format = "fallback", -- not recommended to change
-            },
-            formatters_by_ft = {
-                lua = { "stylua" },
-                fish = { "fish_indent" },
-                sh = { "shfmt" },
-            },
-            -- The options you set here will be merged with the builtin formatters.
-            -- You can also define any custom formatters here.
-            ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
-            formatters = {
-                injected = { options = { ignore_errors = true } },
-                -- # Example of using dprint only when a dprint.json file is present
-                -- dprint = {
-                --   condition = function(ctx)
-                --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
-                --   end,
-                -- },
-                --
-                -- # Example of using shfmt with extra args
-                -- shfmt = {
-                --   prepend_args = { "-i", "2", "-ci" },
-                -- },
-            },
-            }
-            return opts
-        end,
-        config = M.setup,
+        end
     }
 }
