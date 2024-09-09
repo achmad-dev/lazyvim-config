1a98af6c0467eca2cd1028efa464a28ae872339d
achmad <faa04807@gmail.com>
2024-09-10 03:23:42 +0700

    chore(plugin): add chunk, neocord, colorizer

---
+++ lua/plugins/chunk.lua
@@ -0,0 +1,17 @@
+return {
+
+    "shellRaining/hlchunk.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require("hlchunk").setup({
+        chunk = {
+          enable = true,
+          style = "#957FB8",
+        },
+        indent = {
+          enable = true,
+        },
+      })
+    end,
+  }
+  
