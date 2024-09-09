a0dd75c35c4d9d1c7b4975ea92ccda0bfc6f2578
achmad <faa04807@gmail.com>
2024-09-09 18:26:51 +0700

    chore(plugin): update python plugin

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -1,115 +1,117 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = { ensure_installed = { "ninja", "rst" } },
-    },
-    {
-        "neovim/nvim-lspconfig",
-        opts = {
-            servers = {
-            ruff = {
-                cmd_env = { RUFF_TRACE = "messages" },
-                init_options = {
-                settings = {
-                    logLevel = "error",
-                },
-                },
-                keys = {
-                {
-                    "<leader>co",
-                    LazyVim.lsp.action["source.organizeImports"],
-                    desc = "Organize Imports",
-                },
-                },
-            },
-            ruff_lsp = {
-                keys = {
-                {
-                    "<leader>co",
-                    LazyVim.lsp.action["source.organizeImports"],
-                    desc = "Organize Imports",
-                },
-                },
-            },
-            },
-        },
-    },
-    {
-        "neovim/nvim-lspconfig",
-        opts = function(_, opts)
-            local servers = { "pyright", "basedpyright", "ruff", "ruff_lsp", ruff, lsp }
-            for _, server in ipairs(servers) do
-            opts.servers[server] = opts.servers[server] or {}
-            opts.servers[server].enabled = server == lsp or server == ruff
-            end
-        end,
-    },
-    {
-        "nvim-neotest/neotest-python",
-    },
-    {
-        "mfussenegger/nvim-dap-python",
-        -- stylua: ignore
-        keys = {
-            { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method", ft = "python" },
-            { "<leader>dPc", function() require('dap-python').test_class() end, desc = "Debug Class", ft = "python" },
-        },
-        config = function()
-            if vim.fn.has("win32") == 1 then
-            require("dap-python").setup(LazyVim.get_pkg_path("debugpy", "/venv/Scripts/pythonw.exe"))
-            else
-            require("dap-python").setup(LazyVim.get_pkg_path("debugpy", "/venv/bin/python"))
-            end
-        end,
-    },
-    {
-        "linux-cultist/venv-selector.nvim",
-        branch = "regexp", -- Use this branch for the new version
-        cmd = "VenvSelect",
-        enabled = function()
-            return LazyVim.has("telescope.nvim")
-        end,
-        opts = {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "ninja", "rst" } },
+  },
+  {
+    "williamboman/mason.nvim",
+     opts = { ensure_installed = { "debugpy" } },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        ruff = {
+          cmd_env = { RUFF_TRACE = "messages" },
+          init_options = {
             settings = {
-            options = {
-                notify_user_on_venv_activation = true,
+              logLevel = "error",
             },
+          },
+          keys = {
+            {
+              "<leader>co",
+              function() require("lazyvim.util").lsp.organize_imports() end,
+              desc = "Organize Imports",
             },
+          },
         },
-        --  Call config for python files and load the cached venv automatically
-        ft = "python",
-        keys = { { "<leader>cv", "<cmd>:VenvSelect<cr>", desc = "Select VirtualEnv", ft = "python" } },
-    },
-    {
-        "hrsh7th/nvim-cmp",
-        opts = function(_, opts)
-            opts.auto_brackets = opts.auto_brackets or {}
-            table.insert(opts.auto_brackets, "python")
-        end,
-    },
-    {
-        "nvim-neotest/neotest",
-        optional = true,
-        dependencies = {
-            "nvim-neotest/neotest-python",
-        },
-        opts = {
-            adapters = {
-            ["neotest-python"] = {
-                -- Here you can specify the settings for the adapter, i.e.
-                -- runner = "pytest",
-                -- python = ".venv/bin/python",
+        ruff_lsp = {
+          keys = {
+            {
+              "<leader>co",
+              function() require("lazyvim.util").lsp.organize_imports() end,
+              desc = "Organize Imports",
             },
-            },
+          },
         },
+      },
     },
-    {
-        "jay-babu/mason-nvim-dap.nvim",
-        optional = true,
-        opts = {
-            handlers = {
-            python = function() end,
-            },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      local servers = { "pyright", "ruff", "ruff_lsp" }
+      for _, server in ipairs(servers) do
+        opts.servers[server] = opts.servers[server] or {}
+        opts.servers[server].enabled = true -- Enable all the listed LSP servers
+      end
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    -- stylua: ignore
+    keys = {
+      { "<leader>dPt", function() require('dap-python').test_method() end, desc = "Debug Method", ft = "python" },
+      { "<leader>dPc", function() require('dap-python').test_class() end,  desc = "Debug Class",  ft = "python" },
+    },
+    config = function()
+      local path = require("mason-registry").get_package("debugpy"):get_install_path()
+      if vim.fn.has("win32") == 1 then
+        require("dap-python").setup(path .. "/venv/Scripts/python")
+      else
+        require("dap-python").setup(path .. "/venv/bin/python")
+      end
+    end,
+  },
+  {
+    "linux-cultist/venv-selector.nvim",
+    dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+    config = function()
+      require("venv-selector").setup({
+        -- Your options go here
+        -- name = "venv",
+        -- auto_refresh = false
+      })
+    end,
+    event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+    keys = {
+      -- Keymap to open VenvSelector to pick a venv.
+      { "<leader>vs", "<cmd>VenvSelect<cr>" },
+      -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+      { "<leader>vc", "<cmd>VenvSelectCached<cr>" },
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function(_, opts)
+      opts.auto_brackets = opts.auto_brackets or {}
+      table.insert(opts.auto_brackets, "python")
+    end,
+  },
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "nvim-neotest/neotest-python",
+    },
+    opts = {
+      adapters = {
+        ["neotest-python"] = {
+          -- Here you can specify the settings for the adapter, i.e.
+          -- runner = "pytest",
+          -- python = ".venv/bin/python",
         },
+      },
     },
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    optional = true,
+    opts = {
+      handlers = {
+        python = function() end,
+      },
+    },
+  },
 }
